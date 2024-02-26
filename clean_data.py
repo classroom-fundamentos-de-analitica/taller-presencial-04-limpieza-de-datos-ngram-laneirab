07406e524365647b06648175f4063d9a773e0545
Laura  Melissa <laneirab@unal.edu.co>
2024-02-26 11:30:32 -0500

    hol

--- clean_data.py
+++ clean_data.py
@@ -1,62 +1,70 @@
 """Taller evaluable presencial"""
-
 import pandas as pd
 
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
-    data = pd.read_csv(input_file, sep="\t")
-    return data
+
+    df = pd.read_csv(input_file)
+    return df
+
 
 def create_key(df, n):
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
-    
+
     df = df.copy()
 
-    # 1. Copie la columna 'text' a la columna 'fingerprint'
-    df["fingerprint"] = df["text"]
-    # 2. Remueva los espacios en blanco al principio y al final de la cadena
-    df["fingerprint"] = df["fingerprint"].str.strip()
-    # 3. Convierta el texto a minúsculas
-    df["fingerprint"] = df["fingerprint"].str.lower()
-    # 4. Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-    df["fingerprint"] = df["fingerprint"].str.replace("-", "")
-    # 5. Remueva puntuación y caracteres de control
-    df["fingerprint"] = df["fingerprint"].str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
-    # 6. Convierta el texto a una lista de tokens
-    df["fingerprint"] = df["fingerprint"].str.split()
-    # Una el texto sin espacios en blanco
-    df["fingerprint"] = df["fingerprint"].str.join("")
-    # Convierta el texto a una lista de n-gramas
-    df["fingerprint"] = df["fingerprint"].apply(lambda x: [x[i:i+n] for i in range(len(x)-n+1)])
-    # Ordene la lista de n-gramas y remueve duplicados
-    df["fingerprint"] = df["fingerprint"].apply(lambda x: sorted(set(x)))
-    # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
-    df["fingerprint"] = df["fingerprint"].str.join(" ")
     # Copie la columna 'text' a la columna 'key'
-    df["key"] = df["fingerprint"]
+    df["key"] = df["text"]
+
+    # Remueva los espacios en blanco al principio y al final de la cadena
+    df["key"] = df["key"].str.strip()
+
+    # Convierta el texto a minúsculas
+    df["key"] = df["key"].str.lower()
+
+    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
+    df["key"] = df["key"].str.replace("-", "")
+
+    # Remueva puntuación y caracteres de control
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
+
+    # Convierta el texto a una lista de tokens
+    df["key"] = df["key"].str.split()
+
+    # Una el texto sin espacios en blanco
+    df["key"] = df["key"].str.join("")
+   
+    # Convierta el texto a una lista de n-gramas
+    df["key"] = df["key"].map(lambda x: [x[t:t+n-1] for t in range(len(x))])
+
+    # Ordene la lista de n-gramas y remueve duplicados
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
+
     # Convierta la lista de ngramas a una cadena
-    
+    df["key"] = df["key"].str.join("")
+
     return df
+
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
 
-    # 1. Ordene el dataframe por 'fingerprint' y 'text'
-    df=df.sort_values(by=["fingerprint","text"]).copy()
+    # Ordene el dataframe por 'key' y 'text'
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
 
-    # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
-    fingerprints=df.groupby("fingerprint").first().reset_index()
+    # Seleccione la primera fila de cada grupo de 'key'
+    keys = df.drop_duplicates(subset="key", keep="first")
 
-    # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
-    fingerprints=fingerprints.set_index("fingerprint")["text"].to_dict()
+    # Cree un diccionario con 'key' como clave y 'text' como valor
+    key_dict = dict(zip(keys["key"], keys["text"]))
 
-    
-    df["cleaned"]=df["fingerprint"].map(fingerprints)
-    # 4. Cree la columna 'cleaned' usando el diccionario
-
+    # Cree la columna 'cleaned' usando el diccionario
+    df["cleaned"] = df["key"].map(key_dict)
 
     return df
 
